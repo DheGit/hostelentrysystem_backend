1676cb04309d612707caa3cde58b78f35f238b4b
DheGit <bankerdheer@gmail.com>
2022-04-17 18:48:08 +0530

    added person and log models

--- entry_system/models.py
+++ entry_system/models.py
@@ -12,4 +12,24 @@ class Person(models.Model):
         ('F', 'Female'),
         ('O', 'Other')
     ]
-    gender = models.CharField(choices=GENDER_CHOICES, verbose_name="Gender")
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name="Gender")
+
+    def __str__(self):
+        return self.name
+
+
+class Log(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name="logs")
+    
+    hostel_id = models.IntegerField(verbose_name="Hostel")
+
+    time = models.DateTimeField(verbose_name="Entry Time")
+    
+    ACTIVITY_CHOICES = [
+        ('EN', "Entry"),
+        ('EX', "Exit"),
+    ]
+    activity = models.CharField(max_length=2, choices=ACTIVITY_CHOICES, verbose_name="Activity")
+
+    def __str__(self):
+        return f"Log{self.id}"
--- entry_system/urls.py
+++ entry_system/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "entry_system"
 urlpatterns=[
     path("", views.getData, name="getData"),
 ]
