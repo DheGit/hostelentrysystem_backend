0f33d26f043ca8783bc574c52e7817a3aae6b176
DheGit <bankerdheer@gmail.com>
2022-05-09 21:37:20 +0530

    Add erroneous request handling
    Making a request with insufficient input now returns an error response with the detail mentioning so. Making a request which is of the correct format but does not make sense (like duplicate entries) also returns an appropriate response

--- entry_system/views.py
+++ entry_system/views.py
@@ -29,14 +29,46 @@ def registerExit(request):
 
 def registerLog(request, activityType):
      if request.method == "POST":
-        person = Person.objects.get(roll = request.data["roll_no"])
-        hostel_id = request.data["hostel_id"]
+        valid, data = getAndValidateData(request.data, ["roll_no", "hostel_id"])
+        if not valid:
+            return Response({'detail':'Invalid request format'}, status=400)
+
+        studInHostel = personInHostel(data["roll_no"], data["hostel_id"])
+
+        if studInHostel and activityType == 'EN':
+            return Response({'detail':'Can not register entry as student is already in the hostel'}, status=403)
+        if not studInHostel and activityType == 'EX':
+            return Response({'detail':'Can not register exit as student is not in the hostel'}, status=403)
+
+        person = Person.objects.get(roll = data["roll_no"])
+        hostel_id = data["hostel_id"]
         time = timezone.now()
+
 
         new_log = Log(person = person, hostel_id = hostel_id, time=time, activity=activityType)
         new_log.save()
 
-        return Response("Register Successful")
+        return Response({'detail': 'Register Successful'}, status=200)
+
+def personInHostel(roll_no, hostel_no):
+    logs = Log.objects.filter(person__roll=roll_no, hostel_id=hostel_no)
+
+    entries = logs.filter(activity = 'EN').count()
+    exit = logs.filter(activity = 'EX').count()
+
+    return (entries>exit)
+
+def getAndValidateData(data, fields):
+    data_valid = True
+    v_data = dict()
+    for field in fields:
+        if field not in data.keys():
+            data_valid = False
+            v_data[field] = None
+        else:
+            v_data[field]=data[field]
+
+    return (data_valid, v_data)
 
 @api_view(['GET'])
 def getPeopleInside(request, hostel_id):
